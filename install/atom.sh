94c05581928365d3d36c518b2f8701d369f5bbdf
Ryan Boswell <ryan@ryanboswell.com>
2017-03-13 08:23:32 -0400

    Perform a more robust installation and setup of Atom.
    Correct the atom brew package reference, just `atom` doesn’t work.
    Symlink the main configuration files from a synced repo folder.
    Install necessary Atom packages and add-ons.

--- install/atom.sh
+++ install/atom.sh
@@ -1,4 +1,36 @@
-brew install atom
+ATOM_DIR="$HOME/.atom"
+DOTFILES_DIR="$HOME/dotfiles"
+
+# Install Atom application
+brew install Caskroom/cask/atom
+
+# Checkout and setup Atom config directory, or update if it already exists
+if [[ -d "$DOTFILES_DIR/external/.atom" ]]; then
+    git --work-tree="$DOTFILES_DIR/external/.atom" --git-dir="$DOTFILES_DIR/external/.atom/.git" pull origin master
+else
+    git clone https://github.com/ryanboswell/.atom.git $DOTFILES_DIR/external/.atom
+fi
 
 # Alias Atom config directory, if it's there
-[ -d "$DOTFILES_DIR/runcom/.atom" ] && ln -sfv "$DOTFILES_DIR/runcom/.atom" ~
+if [[ -d "$DOTFILES_DIR/external/.atom" ]]; then
+    for file in "init.coffee keymap.cson snippets.cson styles.less";
+        do ln -s $ATOM_DIR/$file $DOTFILES_DIR/external/.atom;
+    done
+fi
+
+
+# Install with apm
+
+packages=(
+  autocomplete-emojis
+  autocomplete-wordpress-hooks
+  editorconfig
+  emmet
+  gist-it
+  greti-syntax
+  pretty-json
+  seti-syntax
+  seti-ui
+)
+
+apm install "${packages[@]}"
